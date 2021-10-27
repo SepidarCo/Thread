72f0f3baf7c36638abfbb61a9f4c321bdcf41823
ali fallah <ali_fhl@yahoo.com>
2021-10-27 16:49:58 +0330

    add simple thread

---
+++ SimpleThread/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread starts here.");
+       
+            Thread backgroundThread = new Thread(new ThreadStart(First));
+            backgroundThread.Start();
+
+
+            Thread backgroundThread2 = new Thread(new ThreadStart(second));
+            backgroundThread2.Start();
+
+            Console.WriteLine("Main thread ends here.");
+            Console.ReadKey();
+        }
+
+
+        public static void First()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void second()
+        {
+            for (int i = 10; i < 20; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
